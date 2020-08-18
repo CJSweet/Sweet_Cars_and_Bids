eaae4f6a2e2fc521ac742fd5959e09506585c43d
Casey Sweet <sweetca72@gmail.com>
2020-08-17 18:26:00 -0700

    cleaner submit UI and radioButtons

--- app/src/main/java/com/example/carsandbids/submit_car/SubmitCarFragment.kt
+++ app/src/main/java/com/example/carsandbids/submit_car/SubmitCarFragment.kt
@@ -4,7 +4,10 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Spinner
+import android.widget.Toast
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProvider
 import com.example.carsandbids.databinding.SubmitCarBinding
 
 class SubmitCarFragment : Fragment() {
@@ -14,7 +17,19 @@ class SubmitCarFragment : Fragment() {
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
+
+
         val binding = SubmitCarBinding.inflate(inflater)
+
+        val submitCarViewModel = ViewModelProvider(this).get(SubmitCarViewModel::class.java)
+
+        // To use the View Model with data binding, you have to explicitly
+        // give the binding object a reference to it.
+        binding.viewModel = submitCarViewModel
+
+        // Specify the fragment view as the lifecycle owner of the binding.
+        // This is used so that the binding can observe LiveData updates
+        binding.lifecycleOwner = viewLifecycleOwner
 
         return binding.root
     }
--- app/src/main/java/com/example/carsandbids/submit_car/SubmitCarViewModel.kt
+++ app/src/main/java/com/example/carsandbids/submit_car/SubmitCarViewModel.kt
@@ -1,8 +1,16 @@
 package com.example.carsandbids.submit_car
 
+import android.graphics.Color
+import android.widget.Toast
 import androidx.lifecycle.ViewModel
 
 class SubmitCarViewModel : ViewModel() {
 
+    var colorB = Color.BLACK
 
+    fun onButtonClick(){
+        //when button is clicked, flip the colors of background and
+        // text
+        println("button pressed")
+    }
 }
