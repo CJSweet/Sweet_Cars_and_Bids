65d189455afbfb93b71e25d44c5db1473a51b977
Casey Sweet <sweetca72@gmail.com>
2020-08-28 17:42:11 -0700

    Starting work on About Fragment

--- app/src/main/java/com/example/carsandbids/about_fragment/AboutFragment.kt
+++ app/src/main/java/com/example/carsandbids/about_fragment/AboutFragment.kt
@@ -5,6 +5,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import com.example.carsandbids.R
 
 class AboutFragment: Fragment() {
 
@@ -13,6 +14,6 @@ class AboutFragment: Fragment() {
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return super.onCreateView(inflater, container, savedInstanceState)
+        return inflater.inflate(R.layout.about_fragment, container, false)
     }
 }
